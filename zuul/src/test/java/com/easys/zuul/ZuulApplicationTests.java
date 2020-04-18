f3aedff1e10bd2ed62695c79572343192dd29bf8
eric-anunciacao <anunciacao.eric@gmail.com>
2020-04-18 18:29:04 -0300

    Adicionando o Zuul ao projeto

---
+++ zuul/src/test/java/com/easys/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easys.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
